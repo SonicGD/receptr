bbc6f9b9d72066948368464f44f5e8cc7cee7eec
SonicGD <sonicgd@gmail.com>
2015-03-08 12:46:17 +0500

    add another host

--- server.go
+++ server.go
@@ -19,7 +19,7 @@ func main() {
     m := martini.New()
     m.Use(MapEncoder)
     m.Use(cors.Allow(&cors.Options{
-        AllowOrigins:     []string{"http://recptr.dev"},
+        AllowOrigins:     []string{"http://recptr.dev", "http://recptr.sonicgd.ru"},
         AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "DELETE", "OPTIONS"},
         /*
           AllowHeaders:     []string{"Origin"},
