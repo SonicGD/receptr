a8f61658e99cd422b3a5d6ba66b56272102d7580
SonicGD <sonicgd@gmail.com>
2015-03-12 22:13:35 +0500

    return recipe on create and go fmt

--- api.go
+++ api.go
@@ -1,56 +1,56 @@
 package main
+
 import (
-    "net/http"
-    "gopkg.in/mgo.v2/bson"
-    "github.com/go-martini/martini"
-    "fmt"
+	"fmt"
+	"github.com/go-martini/martini"
+	"gopkg.in/mgo.v2/bson"
+	"net/http"
 )
 
 func GetRecipes(r *http.Request, enc Encoder) (int, string) {
-    // Get the query string arguments, if any
-    //qs := r.URL.Query()
-    data, err := Recipe{}.all(session, bson.M{})
-    if err != nil {
-        // Invalid id, or does not exist
-        return http.StatusNotFound, Must(enc.Encode(NewError(ErrCodeNotExist, "no recipes found")))
-    }
-    return http.StatusOK, Must(enc.Encode(data))
+	// Get the query string arguments, if any
+	//qs := r.URL.Query()
+	data, err := Recipe{}.all(session, bson.M{})
+	if err != nil {
+		// Invalid id, or does not exist
+		return http.StatusNotFound, Must(enc.Encode(NewError(ErrCodeNotExist, "no recipes found")))
+	}
+	return http.StatusOK, Must(enc.Encode(data))
 }
 
 func toIface(v []*Recipe) []interface{} {
-    if len(v) == 0 {
-        return nil
-    }
-    ifs := make([]interface{}, len(v))
-    for i, v := range v {
-        ifs[i] = v
-    }
-    return ifs
+	if len(v) == 0 {
+		return nil
+	}
+	ifs := make([]interface{}, len(v))
+	for i, v := range v {
+		ifs[i] = v
+	}
+	return ifs
 }
 
 func GetRecipe(enc Encoder, parms martini.Params) (int, string) {
-    id, _ := parms["id"]
-    recipe, err := Recipe{}.one(session, bson.M{"_id": bson.ObjectIdHex(id)})
-    if err != nil {
-        // Invalid id, or does not exist
-        return http.StatusNotFound, Must(enc.Encode(
-        NewError(ErrCodeNotExist, fmt.Sprintf("the recipe with id %s does not exist", id))))
-    }
-    return http.StatusOK, Must(enc.Encode(recipe))
+	id, _ := parms["id"]
+	recipe, err := Recipe{}.one(session, bson.M{"_id": bson.ObjectIdHex(id)})
+	if err != nil {
+		// Invalid id, or does not exist
+		return http.StatusNotFound, Must(enc.Encode(
+			NewError(ErrCodeNotExist, fmt.Sprintf("the recipe with id %s does not exist", id))))
+	}
+	return http.StatusOK, Must(enc.Encode(recipe))
 }
 
 // AddAlbum creates the posted album.
 func AddRecipe(recipe Recipe, enc Encoder) (int, string) {
-    recipe.save(session, recipe)
-    return http.StatusOK, Must(enc.Encode(recipe))
+	recipe = recipe.save(session, recipe)
+	return http.StatusOK, Must(enc.Encode(recipe))
 }
 
 // UpdateAlbum changes the specified album.
 func UpdateRecipe(recipe Recipe, enc Encoder) (int, string) {
-    recipe.update(session, recipe);
-    return http.StatusOK, Must(enc.Encode(recipe))
+	recipe.update(session, recipe)
+	return http.StatusOK, Must(enc.Encode(recipe))
 }
-
 
 // Martini requires that 2 parameters are returned to treat the first one as the
 // status code. Delete is an idempotent action, but this does not mean it should
@@ -58,13 +58,13 @@ func UpdateRecipe(recipe Recipe, enc Encoder) (int, string) {
 // after the request, not the returned status code. So I return a 404 - Not found
 // if the id does not exist.
 func DeleteRecipe(enc Encoder, parms martini.Params) (int, string) {
-    id, _ := parms["id"]
-    recipe, err := Recipe{}.one(session, bson.M{"_id": bson.ObjectIdHex(id)})
-    if err != nil {
-        // Invalid id, or does not exist
-        return http.StatusNotFound, Must(enc.Encode(
-        NewError(ErrCodeNotExist, fmt.Sprintf("the recipe with id %s does not exist", id))))
-    }
-    recipe.delete(session, recipe)
-    return http.StatusNoContent, ""
+	id, _ := parms["id"]
+	recipe, err := Recipe{}.one(session, bson.M{"_id": bson.ObjectIdHex(id)})
+	if err != nil {
+		// Invalid id, or does not exist
+		return http.StatusNotFound, Must(enc.Encode(
+			NewError(ErrCodeNotExist, fmt.Sprintf("the recipe with id %s does not exist", id))))
+	}
+	recipe.delete(session, recipe)
+	return http.StatusNoContent, ""
 }
--- data.go
+++ data.go
@@ -1,77 +1,80 @@
 package main
+
 import (
-    "log"
-    "gopkg.in/mgo.v2"
-    "gopkg.in/mgo.v2/bson"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"log"
 )
 
 type Recipe struct {
-    ID bson.ObjectId `json:"id" bson:"_id,omitempty"`
-    Title string `json:"Title"`
-    Url string `json:"Url"`
-    Picture image `json:"Picture"`
-    Description string `json:"Description"`
-    TimeToCook int `json:"TimeToCook"`
-    Ingredients []Ingredient `json:"Ingredients"`
-    Steps []CookStep `json:"Steps"`
+	ID          bson.ObjectId `json:"id" bson:"_id,omitempty"`
+	Title       string        `json:"Title"`
+	Url         string        `json:"Url"`
+	Picture     image         `json:"Picture"`
+	Description string        `json:"Description"`
+	TimeToCook  int           `json:"TimeToCook"`
+	Ingredients []Ingredient  `json:"Ingredients"`
+	Steps       []CookStep    `json:"Steps"`
 }
 
 func (r Recipe) all(session *mgo.Session, query bson.M) ([]Recipe, error) {
-    c := session.DB("recptr").C("recepies")
-    result := []Recipe{}
-    err := c.Find(query).All(&result)
+	c := session.DB("recptr").C("recepies")
+	result := []Recipe{}
+	err := c.Find(query).All(&result)
 
-    return result, err
+	return result, err
 }
 
 func (r Recipe) one(session *mgo.Session, query bson.M) (Recipe, error) {
-    c := session.DB("recptr").C("recepies")
-    result := Recipe{}
-    err := c.Find(query).One(&result)
-    return result, err
+	c := session.DB("recptr").C("recepies")
+	result := Recipe{}
+	err := c.Find(query).One(&result)
+	return result, err
 }
 
-func (r Recipe) save(session *mgo.Session, recipe Recipe) bool {
-    c := session.DB("recptr").C("recepies")
-    err := c.Insert(recipe)
-    if err != nil {
-        log.Fatal(err)
-    }
-    return true
+func (r Recipe) save(session *mgo.Session, recipe Recipe) Recipe {
+	recipe.ID = bson.NewObjectId()
+	c := session.DB("recptr").C("recepies")
+	err := c.Insert(recipe)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return recipe
 }
 
 func (r Recipe) update(session *mgo.Session, recipe Recipe) bool {
-    c := session.DB("recptr").C("recepies")
-    err := c.Update(bson.M{"_id": recipe.ID}, recipe)
-    if err != nil {
-        log.Fatal(recipe)
-    }
-    return true
+	c := session.DB("recptr").C("recepies")
+	err := c.Update(bson.M{"_id": recipe.ID}, recipe)
+	if err != nil {
+		log.Fatal(recipe)
+	}
+	return true
 }
 
 func (r Recipe) delete(session *mgo.Session, recipe Recipe) bool {
-    c := session.DB("recptr").C("recepies")
-    err := c.Remove(bson.M{"_id": recipe.ID})
-    if err != nil {
-        log.Fatal(err)
-    }
-    return true
+	c := session.DB("recptr").C("recepies")
+	err := c.Remove(bson.M{"_id": recipe.ID})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return true
 }
 
 type Ingredient struct {
-    Title string `json:"Title"`
-    HowMuch string `json:"HowMuch"`
+	Title   string `json:"Title"`
+	HowMuch string `json:"HowMuch"`
 }
 
 type CookStep struct {
-    Title string `json:"Title"`
-    Picture image  `json:"Picture"`
-    Html string `json:"Html"`
+	Title   string `json:"Title"`
+	Picture image  `json:"Picture"`
+	Html    string `json:"Html"`
 }
 
 type image struct {
-    Filetype string `json:"filetype"`
-    Filename string `json:"filename"`
-    Filesize int `json:"filesize"`
-    Base64 string `json:"base64"`
+	Filetype string `json:"filetype"`
+	Filename string `json:"filename"`
+	Filesize int    `json:"filesize"`
+	Base64   string `json:"base64"`
 }
